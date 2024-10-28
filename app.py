6346afa5b2e08ee3645fe7305d82972f407f170a
Hants Williams <hantsawilliams@gmail.com>
2024-10-28 14:46:35 -0400

    changed back port

--- app.py
+++ app.py
@@ -279,4 +279,4 @@ def edit_student(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5005, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
